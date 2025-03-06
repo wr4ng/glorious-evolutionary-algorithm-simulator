ec9271231a48759f7ef1f003dd82057df2632928
Mads Wrang <madscwn@gmail.com>
2025-03-06 10:39:52 +0100

    add test for OneMax

--- server/src/fitness.rs
+++ server/src/fitness.rs
@@ -33,3 +33,37 @@ impl FitnessFunction<Bitstring> for OneMax {
 // instances
 // impl FitnessFunction<Permutation> for TSP {}
 // impl TSP { from_EUC2D() -> TSP }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl Bitstring {
+        fn from_bitstring(bitstring: &str) -> Self {
+            Bitstring::new(bitstring.chars().map(|c| match c {
+                '0' => false,
+                '1' => true,
+                _ => panic!("invalid character: {}", c),
+            })
+            .collect())
+        }
+    }
+
+    #[test]
+    fn test_one_max() {
+        // (bitstring, ones)
+        let testcases = vec![
+            ("0000", 0.0),
+            ("1001010101", 5.0),
+            ("1111111111", 10.0),
+            ("1111100000", 5.0),
+            ("0000011111", 5.0),
+        ];
+
+        for t in testcases {
+            let b = Bitstring::from_bitstring(t.0);
+            let got = OneMax::evaluate(&OneMax, &b);
+            assert_eq!(got, t.1, "expected fitness of {} but got {} on \"{}\"", t.1, got, t.0);
+        }
+    }
+}

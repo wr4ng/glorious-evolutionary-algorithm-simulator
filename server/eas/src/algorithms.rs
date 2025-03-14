6ffc91aa130c8a205fba359135c7e99387bfd308
Mads Wrang <madscwn@gmail.com>
2025-03-14 11:12:39 +0100

    implement cooling schedules for simulated annealing + create example usage

--- server/eas/src/algorithms.rs
+++ server/eas/src/algorithms.rs
@@ -8,7 +8,7 @@ pub mod simulated_annealing;
 // Shared state between all simulations. Probably the type to send to client
 #[derive(Debug)]
 pub struct SimulationState<S: SearchSpace> {
-    pub iteration: usize,
+    pub iteration: u64,
     pub current_solution: S,
     pub current_fitness: f64,
 }

f441584baf416e707a00a4321d9caf9a07355111
Mads Christian Wrang Nielsen <madscwn@gmail.com>
2025-03-22 08:56:41 +0100

    cargo fmt

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -1,35 +1,31 @@
-use axum::{
-    routing::get,
-    http::StatusCode,
-    Json, Router,
-	extract::State,
-};
+use axum::{Json, Router, extract::State, http::StatusCode, routing::get};
 use serde::{Deserialize, Serialize};
-use std::{collections::VecDeque , sync::{Arc, RwLock}};
+use std::{
+    collections::VecDeque,
+    sync::{Arc, RwLock},
+};
 
 #[derive()]
 struct AppState {
-	queue: RwLock<VecDeque<Task>>,
-	finished: RwLock<Vec<Result>>,
+    queue: RwLock<VecDeque<Task>>,
+    finished: RwLock<Vec<Result>>,
 }
 
 type SharedState = Arc<AppState>;
 
 #[tokio::main]
 async fn main() {
-	// tracing_subscriber::fmt::init();
-	let state: SharedState = Arc::new(AppState{
-		queue: RwLock::new(VecDeque::new()),
-		finished: RwLock::new(Vec::new()),
-	});
+    // tracing_subscriber::fmt::init();
+    let state: SharedState = Arc::new(AppState {
+        queue: RwLock::new(VecDeque::new()),
+        finished: RwLock::new(Vec::new()),
+    });
 
     // build our application with a single route
     let app = Router::new()
-    .route("/", get(root))
-	.route("/tasks", 
-		get(get_tasks)
-		.post(create_task))
-	.with_state(state);
+        .route("/", get(root))
+        .route("/tasks", get(get_tasks).post(create_task))
+        .with_state(state);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -40,96 +36,99 @@ async fn root() -> &'static str {
     "Hello, world!"
 }
 
-async fn create_task(State(state): State<SharedState>, Json(request): Json<CreateTaskRequest>) -> StatusCode {
-	let task = Task {
-		id: 1, //TODO random or counting
-		algorithm: request.algorithm,
-		problem: request.problem,
-		stop_cond: request.stop_cond,
-		task_param: request.task_param,
-		parameters: request.parameters,
-		body: request.body,
-	};
-	state.queue.write().unwrap().push_back(task);
+async fn create_task(
+    State(state): State<SharedState>,
+    Json(request): Json<CreateTaskRequest>,
+) -> StatusCode {
+    let task = Task {
+        id: 1, //TODO random or counting
+        algorithm: request.algorithm,
+        problem: request.problem,
+        stop_cond: request.stop_cond,
+        task_param: request.task_param,
+        parameters: request.parameters,
+        body: request.body,
+    };
+    state.queue.write().unwrap().push_back(task);
 
-	StatusCode::CREATED
+    StatusCode::CREATED
 }
 
 async fn get_tasks(State(state): State<SharedState>) -> (StatusCode, Json<TasksReturn>) {
-	let tasks = state.queue.read().unwrap().clone().into_iter().collect();
-	let finished = state.finished.read().unwrap().clone();
-	let returns = TasksReturn{
-		queued: tasks,
-		finished,
-	};
-	(StatusCode::ACCEPTED, Json(returns))
+    let tasks = state.queue.read().unwrap().clone().into_iter().collect();
+    let finished = state.finished.read().unwrap().clone();
+    let returns = TasksReturn {
+        queued: tasks,
+        finished,
+    };
+    (StatusCode::ACCEPTED, Json(returns))
 }
 
 #[derive(Deserialize)]
 struct CreateTaskRequest {
-	algorithm: Algorithm,
-	problem: Problem,
-	stop_cond: StopCondition,
-	task_param: TaskParameters,
-	parameters: AlgoParameters,
-	body: String,
+    algorithm: Algorithm,
+    problem: Problem,
+    stop_cond: StopCondition,
+    task_param: TaskParameters,
+    parameters: AlgoParameters,
+    body: String,
 }
 
 #[derive(Serialize, Clone)]
 struct Task {
-	id:  usize,
-	algorithm: Algorithm,
-	problem: Problem,
-	stop_cond: StopCondition,
-	task_param: TaskParameters,
-	parameters: AlgoParameters,
-	body: String,
+    id: usize,
+    algorithm: Algorithm,
+    problem: Problem,
+    stop_cond: StopCondition,
+    task_param: TaskParameters,
+    parameters: AlgoParameters,
+    body: String,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
 enum Algorithm {
-	OnePlusOneEA,
-	SimulatedAnnealing,
-	ACO,
+    OnePlusOneEA,
+    SimulatedAnnealing,
+    ACO,
 }
 #[derive(Deserialize, Serialize, Clone)]
 enum Problem {
-	OneMax,
-	TSP,
-	LeadginOnes,
+    OneMax,
+    TSP,
+    LeadginOnes,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
 struct StopCondition {
-	max_time: Option<usize>,
-	max_iterations: Option<usize>,
-	max_iterations_since_improvement: Option<usize>,
-	requested_fitness: Option<usize>,
+    max_time: Option<usize>,
+    max_iterations: Option<usize>,
+    max_iterations_since_improvement: Option<usize>,
+    requested_fitness: Option<usize>,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
 struct TaskParameters {
-	repetitions: Option<usize>,
-	data_interval: Option<usize>,
-	measure_time: Option<bool>,
-	save_running_solutions: Option<bool>,
-	save_running_fitness: Option<bool>
+    repetitions: Option<usize>,
+    data_interval: Option<usize>,
+    measure_time: Option<bool>,
+    save_running_solutions: Option<bool>,
+    save_running_fitness: Option<bool>,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
 struct AlgoParameters {
-	//TODO
+    //TODO
 }
 
 #[derive(Serialize, Clone)]
 struct Result {
-	id: usize,
-	algorithm: Algorithm,
-	problem: Problem,	
+    id: usize,
+    algorithm: Algorithm,
+    problem: Problem,
 }
 
 #[derive(Serialize)]
 struct TasksReturn {
-	queued: Vec<Task>,
-	finished: Vec<Result>,
+    queued: Vec<Task>,
+    finished: Vec<Result>,
 }

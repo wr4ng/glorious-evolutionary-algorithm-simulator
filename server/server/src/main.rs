66514df70f71846e3f85a97372cfd96ef95a048e
Mads Wrang <madscwn@gmail.com>
2025-04-20 20:51:22 +0200

    fix: hardcoded comparison not working with both maximizing and minimizing fitness functions

--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -100,7 +100,8 @@ async fn create_task(
             }
             // If optimal solution is provided, stop if it is reached
             if let Some(optimal) = task.stop_cond.optimal_fitness {
-                if optimal >= runner.current_fitness() {
+                //TODO: Use fitness function to compare to avoid exact matching
+                if optimal == runner.current_fitness() {
                     break;
                 }
             }
